d523f92dd24fe19929a67a9fed9899f72f998357
raghavsawhney28 <raghavsawhney28@gmail.com>
2024-07-04 16:41:59 +0530

    .

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error  {
         statusCode,
         message=[],
         errors = [],
-        statck= ""
+        stack= ""
 
     ){
         super(message)
@@ -13,8 +13,8 @@ class ApiError extends Error  {
         this.sucess = false;
         this.errors = errors
 
-        if (statck) {
-            this.stack = statck
+        if (stack) {
+            this.stack = stack
         } else {
             Error.captureStackTrace(this, this.constructor)
         }
