6fc3fcf06265460a60bc4c924793e80d6eea4c24
raghavsawhney28 <raghavsawhney28@gmail.com>
2024-07-06 01:55:58 +0530

    middleware

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,7 +1,23 @@
 import asyncHandler from "../utils/asyncHandler.js";
-import ApiError from "../utils/ApiError.js";
+import  ApiError from "../utils/ApiError.js"
 import { User } from "../models/user/user.model.js";
 import ApiResponse from "../utils/ApiResponse.js";
+
+const generageAccessAndRefreshTokens = async(userId) {
+  try {
+    const user = await User.findById(userId)
+    const accessToken = user.generateAccessToken()
+    const refreshToken = user.generateRefreshToken()
+
+    user.refreshToken = refreshToken
+    await user.save({ validateBeforeSave: false })
+
+    return{accessToken, refreshToken}
+
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong while generating refresh and access token")
+  }
+}
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -10,7 +26,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({ email });
+  const existedUser = await User.findOne({ email });
 
   if (existedUser) {
     throw new ApiError(409, "User with this email already exists");
@@ -19,8 +35,8 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     fullName,
     email,
-    password,
-    username: username.toLowerCase(),
+    password
+    
   });
 
   const createdUser = await User.findById(user._id).select(
@@ -38,4 +54,76 @@ const registerUser = asyncHandler(async (req, res) => {
 
 });
 
-export default registerUser;
+const loginUser = asyncHandler(async (req,res) => {
+
+    const {email, password} = req.body
+
+    if (!email) {
+      throw new ApiError(400, "email is required")
+    }
+
+    const user = await User.findOne ({email})
+
+    if (!user) {
+      throw new ApiError(404, "User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+      throw new ApiError(401, "Invalid user credentials")
+    }
+
+    const{accessToken, refreshToken} = await generageAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).
+    select("-password -refreshToken")
+
+    const options = {
+      httpOnly: true,
+      secure: true
+    }
+
+    return res.status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser, accessToken,
+          refreshToken
+        },
+        "User logged In Successfully"
+      )
+    )
+
+
+})
+
+const logoutUser = asyncHandler(async(req, res) =>{
+    await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $set: {
+          refreshToken: undefined
+        }
+      },
+      {
+        new: true
+      }
+    )
+
+    const options = {
+      httpOnly: true,
+      secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"))
+})
+
+export  {registerUser, loginUser, logoutUser};
--- backend/models/user/user.model.js
+++ backend/models/user/user.model.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         trim: true
     },
-    fullname: {
+    fullName: {
         type: String,
         required: true,
         trim: true
@@ -40,7 +40,7 @@ userSchema.methods.generateAccessToken = function(){
         _id: this._id,
         email: this.email,
         username: this.username,
-        fullname: this.fullname
+        fullName: this.fullName
         
     },
     process.env.ACCESS_TOKEN_SECRET,
