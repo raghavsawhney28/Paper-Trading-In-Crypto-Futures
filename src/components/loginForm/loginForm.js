327f7923e15bcd267d27fa4e0d5b886634ca9b6e
Raghav <128964036+raghavsawhney28@users.noreply.github.com>
2024-07-28 01:30:41 +0530

    Update loginForm.js

--- src/components/loginForm/loginForm.js
+++ src/components/loginForm/loginForm.js
@@ -24,7 +24,7 @@ const LoginForm = () => {
 
   const onSubmit = async (data) => {
     try {
-      const response = await axios.post("/api/v1/users/login", data);
+      const response = await axios.post("http://localhost:8000/api/v1/users/login", data);
       console.log("Login successful:", response.data);
       a.setIsLoggedIn(true)
       navigate("/homepage");
