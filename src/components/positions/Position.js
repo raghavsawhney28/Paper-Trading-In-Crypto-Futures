594f6ccb89f33df706a34d276360622011e2d6f4
raghavsawhney28 <raghavsawhney28@gmail.com>
2024-07-28 17:03:17 +0530

    .

--- src/components/positions/Position.js
+++ src/components/positions/Position.js
@@ -108,7 +108,7 @@ const Positions = () => {
 
   const logout = async () => {
     try {
-      const response = await fetch('api/v1/users/logout', {
+      const response = await fetch('https://paper-trading-in-crypto-futures-backend.onrender.com/api/v1/users/logout', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
